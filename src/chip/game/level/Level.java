61ddbdc1071a33bc640fcf803f3f5768c061d614
rommykurniawan <kurniawanwijayarommy@gmail.com>
2014-11-21 11:23:42 +0700

    Update Level.java

--- src/chip/game/level/Level.java
+++ src/chip/game/level/Level.java
@@ -10,7 +10,7 @@ import java.awt.Point;
 /**
  *  Kelas Level yang merupakan kelas Parent
  * 
- * @author Albert Kamord,Rommy Kurniawan Wijaya
+ * @author Albert Kamord,Rommy Kurniawan Wijaya1
  */
 public class Level {
     /**
